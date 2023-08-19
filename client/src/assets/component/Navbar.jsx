68b8428a30be6120ca96b9ed503bbdb208e357bb
gopinath <gopinaths2301@gmail.com>
2023-08-20 02:18:56 +0530

    navbar outline

---
+++ client/src/assets/component/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo from '/logo.svg'
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import '../../styles/components/--Navbar.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+
+const Navbar = () => {
+
+    return (
+        <>
+            <nav className='nav'>
+                <ul className='nav__list'>
+                    <li className='nav__item'><a className='nav__link'><img src={Logo} className='nav__link-logo' alt="" /></a></li>
+                    <li className='nav__item'><a className='nav__link '>
+                        <input className='nav__link--search' type="text" /></a></li>
+                    <li className='nav__item'><a className='nav__link'><Button className='nav__login-button' variant="contained">Login</Button></a></li>
+                    <li className='nav__item'><a className='nav__link'><Button className='nav__logout-button' variant="outlined">Logout</Button></a></li>
+                </ul>
+            </nav>
+
+
+        </>
+    )
+}
+
+export default Navbar
