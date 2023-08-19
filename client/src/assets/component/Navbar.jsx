89334e60888d5a32302e4a511ffaca86266c9992
gopinath <gopinaths2301@gmail.com>
2023-08-20 02:30:13 +0530

    navbar rough

--- client/src/assets/component/Navbar.jsx
+++ client/src/assets/component/Navbar.jsx
@@ -8,16 +8,15 @@ import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 
 const Navbar = () => {
-
+    const user = false;
     return (
         <>
             <nav className='nav'>
                 <ul className='nav__list'>
                     <li className='nav__item'><a className='nav__link'><img src={Logo} className='nav__link-logo' alt="" /></a></li>
                     <li className='nav__item'><a className='nav__link '>
-                        <input className='nav__link--search' type="text" /></a></li>
-                    <li className='nav__item'><a className='nav__link'><Button className='nav__login-button' variant="contained">Login</Button></a></li>
-                    <li className='nav__item'><a className='nav__link'><Button className='nav__logout-button' variant="outlined">Logout</Button></a></li>
+                        <input className='nav__link--search' placeholder='search' type="text" /></a></li>
+                    {user ? (<li className='nav__item'><a className='nav__link'><Button className='nav__logout-button' variant="outlined">Logout</Button></a></li>) : (<li className='nav__item'><a className='nav__link'><Button className='nav__login-button' variant="contained">Login</Button></a></li>)}
                 </ul>
             </nav>
 
