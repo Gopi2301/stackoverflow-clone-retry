181deed0d1853f11c3afe3d3213e5759aca0a6b7
Gopi2301 <gopinaths2301@gmail.com>
2023-08-21 22:36:32 +0530

    pc back

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import logo from '../assets/logo.svg'
+import '../styles/components/--navbar.scss'
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+
+        </div >
+    )
+}
+
+export default Navbar
